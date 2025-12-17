cc9f696cf87584b43ccb34940fee0b369fe46b7f
Vishnu Prasad <107107028+vishnuprasadkm@users.noreply.github.com>
2025-12-17 19:55:59 +0530

    Update models.py

--- FastAPI/FastAPI - The Complete Course 2026 (Beginner + Advanced)/Section 3/models.py
+++ FastAPI/FastAPI - The Complete Course 2026 (Beginner + Advanced)/Section 3/models.py
@@ -1,5 +1,20 @@
 from database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Users(Base):
+
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String, unique=True)
+    email = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    crypt_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String, default="user")
+
 
 class Todos(Base):
 
@@ -10,3 +25,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
